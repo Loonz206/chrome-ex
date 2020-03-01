835e522ca725403668b9e68c39dca9c68dcb71a2
Lenny Peters <mr.lennypeters@gmail.com>
2020-03-01 10:49:40 -0800

    scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+/* eslint-disable prettier/prettier */
+const path = require('path');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  devtool: 'cheap-module-source-map',
+  entry: "./src/index.jsx",
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    // eslint-disable-next-line prettier/prettier
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        // eslint-disable-next-line prettier/prettier
+        use: [{
+          loader: "html-loader"
+        }]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      title: "boilerplate", // change this to your app title
+      meta: {
+        charset: "utf-8",
+        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+        "theme-color": "#000000"
+      },
+      manifest: "manifest.json",
+      hash: true
+    }),
+    new CopyWebpackPlugin([{
+        from: './src/manifest.json',
+        to: './',
+        flatten: true
+      },
+      {
+        from: './src/assets/img/icons/*',
+        to: './',
+        flatten: true
+      }
+    ], {
+      copyUnmodified: true
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
